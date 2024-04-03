8d10fa39aa4caae9b86984c79852f6300039c371
brunacdp <bruna.campos@gmail.com>
2024-04-03 14:18:09 -0300

    first commit

---
+++ js/cart.js
@@ -0,0 +1,14 @@
+var valorTotal = [0,0];
+var valorProduto = [50.00,30.00]
+var qtd = [0,0];
+
+function adicionarItem(item){
+  var quantidade = document.getElementById("quantidade" + item)
+  var total = document.getElementById("total" + item)
+  qtd[item] += 1;
+  valorTotal[item] = Number.parseFloat(valorProduto[item]) * qdt[item];
+  quantidade.innerHTML = qtd[item];
+  total.innerHTML = valorTotal[item].toFixed(2);
+
+
+}
